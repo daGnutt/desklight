cd830ff829a8aa2f1cd4911311261e38998c8105
Gnutt Halvordsson <gnutt@halvordsson.se>
2018-05-26 14:33:37 +0200

    Added timeoutmanagement for incoming connections

--- node/node.ino
+++ node/node.ino
@@ -5,6 +5,17 @@
 #include <WiFiClient.h>
 #include <Adafruit_NeoPixel.h>
 
+#if BOARD_DEVELOPMENT
+#define DEV_SERIAL_PRINT( x ) Serial.print( x )
+#define DEV_SERIAL_PRINTLN( x ) Serial.println( x )
+#define DEV_SERIAL_PRINTF( x, ... ) Serial.printf( x, __VA_ARGS__ );
+#else
+#define DEV_SERIAL_PRINT( x )
+#define DEV_SERIAL_PRINTLN( x )
+#define DEV_SERIAL_PRINTF( x, ... )
+#endif
+
+
 const byte MAC_LENGTH = 6;
 const byte BYTESPERPIXEL = 4;
 
@@ -14,10 +25,9 @@ Adafruit_NeoPixel GPixels( NUM_PIXELS, PIN_PIXELS, NEO_GRB + NEO_KHZ800 );
 
 enum class ERequestStatus : unsigned int {
   RS_Success = 0,
-  RS_OtherError,
-  RS_ToLargePayload,
-  RS_ToSmallPayload,
-  RS_BadMac
+  RS_BadMac,
+  RS_BadPayload,
+  RS_Timeout
 };
 
 // Simple software timer class.
@@ -116,10 +126,27 @@ void SendHeartbeat()
 Timer GHeartbeatTimer;
 
 void setup() {
+#if BOARD_DEVELOPMENT
+  Serial.begin( 115200 );
+  delay( 10 );
+#endif
+
+  DEV_SERIAL_PRINTLN( "Connecting to " );
+  DEV_SERIAL_PRINTLN( WIFI_SSID );
   WiFi.begin( WIFI_SSID, WIFI_PASSWORD );
   while( WiFi.status() != WL_CONNECTED ) {
-    delay( 10 );
+    delay( 100 );
+    DEV_SERIAL_PRINT( "." );
   }
+  DEV_SERIAL_PRINTLN( "" );
+  DEV_SERIAL_PRINTLN( "WiFi connected" );
+  DEV_SERIAL_PRINTLN( " IP: " );
+  DEV_SERIAL_PRINTLN( WiFi.localIP() );
+
+  byte mac[ 6 ];
+  WiFi.macAddress( mac );
+  DEV_SERIAL_PRINTF( " MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", mac[ 0 ], mac[ 1 ], mac[ 2 ], mac[ 3 ], mac[ 4 ], mac[ 5 ] );
+
 
   GRequestServer.begin();
 
@@ -133,11 +160,23 @@ void setup() {
 
 ERequestStatus GHandleConnection( WiFiClient connection )
 {
+  DEV_SERIAL_PRINTLN( "Handling incoming connection" );
   if( connection.connected() )
   {
+    unsigned long startOfConnection = millis();
+    unsigned long currentTime;
     while( connection.available() < MAC_LENGTH )
     {
+      if( connection.status() == 0 )
+      {
+        return ERequestStatus::RS_BadMac;
+      }
       delay( 1 );
+      currentTime = millis();
+      if( currentTime - startOfConnection > 1000 )
+      {
+        return ERequestStatus::RS_Timeout;
+      }
     }
 
     byte device_mac[ 6 ];
@@ -146,21 +185,35 @@ ERequestStatus GHandleConnection( WiFiClient connection )
     byte mac[ MAC_LENGTH ];
     uint8_t mac_length = connection.read( mac, MAC_LENGTH );
 
+    DEV_SERIAL_PRINTLN( "Comparing following MAC Address" );
+    DEV_SERIAL_PRINTF( " Device  MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", device_mac[ 0 ], device_mac[ 1 ], device_mac[ 2 ], device_mac[ 3 ], device_mac[ 4 ], device_mac[ 5 ] );
+    DEV_SERIAL_PRINTF( " Payload MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", mac[ 0 ], mac[ 1 ], mac[ 2 ], mac[ 3 ], mac[ 4 ], mac[ 5 ] );
+
     if( device_mac[ 0 ] != mac[ 0 ]
      || device_mac[ 1 ] != mac[ 1 ]
      || device_mac[ 2 ] != mac[ 2 ]
      || device_mac[ 3 ] != mac[ 3 ]
      || device_mac[ 4 ] != mac[ 4 ]
-     || device_mac[ 5 ] != mac[ 5 ]
-     || device_mac[ 6 ] != mac[ 6 ] )
+     || device_mac[ 5 ] != mac[ 5 ] )
      {
       return ERequestStatus::RS_BadMac;
      }
     while( connection.available() )
     {
+      startOfConnection = millis();
       while( connection.available() < BYTESPERPIXEL )
       {
+        if( connection.status() == 0 )
+        {
+          return ERequestStatus::RS_BadPayload;
+        }
+                
         delay( 1 );
+        currentTime = millis();
+        if( currentTime - startOfConnection > 1000 )
+        {
+         return ERequestStatus::RS_Timeout;
+        }
       }
       byte pixelPayload[ BYTESPERPIXEL ];
       uint8_t pixelPayloadLenght = connection.read( pixelPayload, BYTESPERPIXEL );
