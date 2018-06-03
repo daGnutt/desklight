4f1de2ebbc4dca6aaa68b408f3f3f5515a273d0f
Gnutt Halvordsson <gnutt@halvordsson.se>
2018-06-03 17:22:43 +0200

    Extraceted the timer code from node
    Moved it into separate header-file, to be easier to reuse.

--- node/node.ino
+++ node/node.ino
@@ -1,3 +1,4 @@
+#include "timer.h"
 #include "settings.h"
 
 #include <ESP8266WiFi.h>
@@ -28,75 +29,6 @@ enum class ERequestStatus : unsigned int {
   RS_BadMac,
   RS_BadPayload,
   RS_Timeout
-};
-
-// Simple software timer class.
-class Timer
-{
-public:
-  void (*Callback_Start)();
-  void (*Callback_Tick)();
-  void (*Callback_Stop)();
-
-public:
-  Timer() :
-    Callback_Start( nullptr ),
-    Callback_Tick( nullptr ),
-    Callback_Stop( nullptr ),
-    mStartTime( 0 ),
-    mInterval( -1 ),
-    mLooping( false )
-  {}
-
-  /** @param interval Time in ms. */
-  void Start( int interval, bool looping )
-  {
-    if( interval >= 0 )
-    {
-      mInterval = interval;
-      mLooping = looping;
-      mStartTime = millis();
-    
-      if( Callback_Start ) Callback_Start();
-    }
-  }
-  
-  void Update()
-  {
-    if( IsActive() )
-    {
-      unsigned long now = millis();
-      if( now - mStartTime > mInterval )
-      {
-        if( Callback_Tick ) Callback_Tick();
-        
-        if( !mLooping )
-          Stop();
-        else
-          mStartTime = now;
-      }
-    }
-  }
-
-  void Stop()
-  {
-    if( IsActive() )
-    {
-      mInterval = -1;
-
-      if( Callback_Stop ) Callback_Stop();
-    }
-  }
-
-  inline bool IsActive() const
-  {
-    return mInterval >= 0;
-  }
-  
-private:
-  unsigned long mStartTime;
-  int mInterval;
-  bool mLooping;
 };
 
 void SendHeartbeat()
