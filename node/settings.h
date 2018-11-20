e02364531176da72b08fa24089a8727c671d253f
Gnutt Halvordsson <gnutt@halvordsson.se>
2018-11-20 08:33:39 +0100

    Added example settings

---
+++ node/settings.h
@@ -0,0 +1,14 @@
+#define BOARD_DEVELOPMENT 1
+
+const char*         WIFI_SSID             = "";
+const char*         WIFI_PASSWORD         = "";
+
+const unsigned int  SERVER_PORT           = 5007;
+
+const char*         CHECKIN_IP            = "172.16.0.5";
+const unsigned int  CHECKIN_PORT          = 5007;
+const unsigned int  HEARTBEAT_INTERVAL    = 5000;
+
+const int           PIN_PIXELS            = 2;
+const unsigned int  NUM_PIXELS            = 190;
+
